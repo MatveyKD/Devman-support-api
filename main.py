844dcf7b2577dd0038ea036de77c6dd4f693bf56
MatveyKD <97842332+MatveyKD@users.noreply.github.com>
2023-04-05 18:18:46 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from dotenv import load_dotenv
+from google.cloud import dialogflow
 
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
@@ -25,8 +26,34 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def answer(update: Update, context: CallbackContext) -> None:
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(os.getenv("PROJECT_ID"), os.getenv("SESSION_ID"))
+    print(f"Session path: {session}\n")
+
+    for text in [update.message.text]:
+        text_input = dialogflow.TextInput(text=text, language_code="ru-RU")
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={
+                "session": session,
+                "query_input": query_input
+            }
+        )
+
+        print("=" * 20)
+        print(f"Query text: {response.query_result.query_text}")
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print(f"Fulfillment text: {response.query_result.fulfillment_text}\n")
+        update.message.reply_text(response.query_result.fulfillment_text)
 
 
 def main() -> None:
@@ -36,7 +63,7 @@ def main() -> None:
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text, answer))
 
     updater.start_polling()
 

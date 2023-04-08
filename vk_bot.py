ca20262410cd900136bf82bb2a9bafbad55b922e
MatveyKD <97842332+MatveyKD@users.noreply.github.com>
2023-04-08 11:25:22 +0300

    Add fallback ignoring

--- vk_bot.py
+++ vk_bot.py
@@ -35,11 +35,12 @@ def answer(event, vk_api):
     )
     print(f"Fulfillment text: {response.query_result.fulfillment_text}\n")
 
-    vk_api.messages.send(
-        user_id=event.user_id,
-        message=response.query_result.fulfillment_text,
-        random_id=random.randint(1, 1000)
-    )
+    if not response.query_result.intent.is_fallback:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=response.query_result.fulfillment_text,
+            random_id=random.randint(1, 1000)
+        )
 
 
 if __name__ == "__main__":

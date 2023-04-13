ebda5e06e2294c26b2187df938d63b331606384a
MatveyKD <97842332+MatveyKD@users.noreply.github.com>
2023-04-13 11:11:07 +0300

    delete all prints

--- QuestionsAnswerBot.py
+++ QuestionsAnswerBot.py
@@ -5,7 +5,6 @@ def answer_message(message_text, project_id, session_id):
     session_client = dialogflow.SessionsClient()
 
     session = session_client.session_path(project_id, session_id)
-    print(f"Session path: {session}\n")
 
     text_input = dialogflow.TextInput(text=message_text, language_code="ru-RU")
 

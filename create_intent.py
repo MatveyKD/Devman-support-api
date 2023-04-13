dbc8d3a870f492dfe2f46784d6e53d42972a105d
MatveyKD <97842332+MatveyKD@users.noreply.github.com>
2023-04-13 12:44:31 +0300

    Replace questions file path to env

--- create_intent.py
+++ create_intent.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     load_dotenv()
 
     data = {}
-    with open("questions.json", "r", encoding="UTF-8") as reading_file:
+    with open(os.getenv("QUESTIONS_FILE_PATH"), "r", encoding="UTF-8") as reading_file:
         data = json.load(reading_file)
 
     for question_text, question_data in data.items():
